12f20ce95c3ceca17ef23ea549647b0e1a53910f
marek <march@gmx.fr>
2021-07-19 18:00:55 +0200

    fonction chiffre aleatoire associée à la video

--- script.js
+++ script.js
@@ -2,16 +2,33 @@
 const video = document.querySelector('video');
 /* On sélectionne le bouton "video" */
 const btnVideo = document.getElementById('btn-roll');
-
 /* Au clic*/ 
 btnVideo.addEventListener('click', () => {
 /* On affiche la video */
  video.style.visibility = 'visible';
-/* On lance la video */
+/* La video est lancée */
   video.play();
 });
-
-/* A la fin de la video, on cache la video */
+/* A la fin de la video, on peut cacher la video avec */
 // video.onended = () => {
-//     video.style.visibility = 'hidden';
-//   }
+//  video.style.visibility = 'hidden';
+// }
+
+
+const roll = document.getElementById('btn-roll');
+let faces = ["videos/1.mp4", "videos/2.mp4", "videos/3.mp4", "videos/4.mp4","videos/5.mp4","videos/6.mp4"];
+
+// fonction donnant un chiffre aléatoire entre 1 et 6 et associé au dé
+const dice = () => {
+  let valueDice = Math.floor(Math.random() *  faces.length);
+  let pointsDice = faces.indexOf(faces[number]) + 1;
+// affichage des videos correspondantes
+}
+
+roll.addEventListener('click', dice);
+  
+
+
+
+
+
